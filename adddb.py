d5133a69ae8c8cbda7c0558764235f0b979cf0b8
30padavan10 <30padavan10@gmail.com>
2019-12-24 16:43:23 +0800

    Append to db by search

--- adddb.py
+++ adddb.py
@@ -1,30 +1,13 @@
 import psycopg2
-import re
-
-def connect():
-    conn = psycopg2.connect("dbname=testdb user=postgres password=postgres")
-    cur = conn.cursor()
-    return cur
-
-def exit():
-    cur.close()
-    conn.close()
-
-def datainput():
-    data = input('Search: ')
-    return data
-
+import avito
 
 def add_to_db(table, data):
+    table = table.replace('%20', '_')
     conn = psycopg2.connect("dbname=testdb user=postgres password=postgres")
     cur = conn.cursor()
-    #try:
-    #    sql = "SELECT * FROM {};".format(table)
-    #    cur.execute(sql)
-    #except UndefinedTable:
     sql_create = "CREATE TABLE IF NOT EXISTS {} (name varchar(150), href varchar(150), city varchar(150), price int, date date, description text)".format(table)
-    print(sql_create)
     cur.execute(sql_create)
+    conn.commit()
     for post in data:
         name, price, href, date = post
         city = 'ekaterinburg' if 'ekaterinburg' in href else 'other'
@@ -36,26 +19,33 @@ def add_to_db(table, data):
         except TypeError:
             sql_insert_table = "insert into {} (name, price, href, city, date) values ('{}', {}, '{}', '{}', '{}')".format(table, name, price, href, city, date)
             cur.execute(sql_insert_table)
-            print('Новый:  '+name+'  '+str(price)+'  '+href+'  '+city)
+            conn.commit()
+            print('Новый:  '+name+'  '+str(price)+'  '+city)
         else:
-            try:
-                new_desc=str(last_price)+'р '+str(last_date)+' '+description
-            except TypeError:
-                new_desc=str(last_price)+'р '+str(last_date)
-            finally:
-                sql_del_entry = "DELETE FROM {} WHERE href = '{}'".format(table, href)
-                cur.execute(sql_del_entry)
-                sql_update_table = "insert into {} (name, price, href, city, date, desc) values ('{}', {}, '{}', '{}' '{}', '{}')".format(table, name, price, href, city, date, new_desc)
-                cur.execute(sql_update_table)
-                conn.commit()
-                print('Изменился '+name+'  '+str(price)+'  '+href+'  '+city+'  '+new_desc)
+            if last_price != price:
+                try:
+                    new_desc=str(last_price)+'р '+str(last_date)+' '+description
+                except TypeError:
+                    new_desc=str(last_price)+'р '+str(last_date)
+                finally:
+                    sql_del_entry = "DELETE FROM {} WHERE href = '{}'".format(table, href)
+                    cur.execute(sql_del_entry)
+                    sql_update_table = "insert into {} (name, price, href, city, date, description) values ('{}', {}, '{}', '{}', '{}', '{}')".format(table, name, price, href, city, date, new_desc)
+                    cur.execute(sql_update_table)
+                    conn.commit()
+                    print('Изменился '+name+'  '+str(price)+'  '+city+'  '+new_desc)
+            else:
+                pass
+    cur.close()
+    conn.close()
                 
 
 
-data = [['\n Маска для сноуборда и горных лыж Oakley Splice\n ', 5000, '/ekaterinburg/sport_i_otdyh/maska_dlya_snouborda_i_gornyh_lyzh_oakley_splice_1774700923', '2019-12-19'], ['\n Женские Горнолыжные очки Oakley\n ', 2000, '/ekaterinburg/sport_i_otdyh/zhenskie_gornolyzhnye_ochki_oakley_1135058010', '2019-12-19'], ['\n Маска Oakley O2XL Matte White/Persimmon\n ', 3500, '/ekaterinburg/sport_i_otdyh/maska_oakley_o2xl_matte_whitepersimmon_1148321556', '2019-12-19']]
+data = [['\n Маска для сноуборда и горных лыж Oakley Splice\n ', 5000, '/ekaterinburg/sport_i_otdyh/maska_dlya_snouborda_i_gornyh_lyzh_oakley_splice_1774700923', '2019-12-21'], ['\n Женские Горнолыжные очки Oakley\n ', 2000, '/ekaterinburg/sport_i_otdyh/zhenskie_gornolyzhnye_ochki_oakley_1135058010', '2019-12-19'], ['\n Маска Oakley O2XL Matte White/Persimmon\n ', 3500, '/ekaterinburg/sport_i_otdyh/maska_oakley_o2xl_matte_whitepersimmon_1148321556', '2019-12-19']]
 
 if __name__ == '__main__':
-    add_to_db('oakley', data)
+    quest = avito.Question()
+    add_to_db(quest, avito.ParserFromAvito(quest))
 
 
 
--- avito.py
+++ avito.py
@@ -1,32 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
-question = input('Поиск: ')
-search_line = '%20'.join(question.split())
-url = 'https://www.avito.ru/ekaterinburg/sport_i_otdyh/zimnie_vidy_sporta?cd=1&q='+search_line
-r = requests.get(url)
+from datetime import datetime
 
-soup = BeautifulSoup(r.content.decode('utf-8'))
-search = soup.find_all('div', {'class': 'snippet-horizontal item item_table clearfix js-catalog-item-enum item-with-contact js-item-extended'})
 
-search_list = []
-for items in search:
-    total_dict = {}
-    #Ищем внутри одного из div-ов внутренний div, в нем атрибут а, и из него вытаскиваем ссылку
-    href = items.find('div', {'class': 'snippet-title-row'}).find('a').get('href')
-    total_dict.update({'href':href})
-    #Тоже самое, но достаем текст ссылки
-    name = items.find('div', {'class': 'snippet-title-row'}).find('a').text
-    str_name = str(name)
-    total_dict.update({'name':name})
-    #Ищем цену
-    price = items.find('span', {'class': 'price'}).text
-    total_dict.update({'price':price})
-    search_list.append(total_dict)
-    if 'в Екатеринбурге' in str_name:
-        ekb_total_dict = total_dict
-        search_list.append(ekb_total_dict)
+def Question():
+    question = input('Поиск: ')
+    search_line = '%20'.join(question.split())
+    return search_line
 
-print(search_list)
+def ParserFromAvito(search_line):
+    date = str(datetime.now().date())
+
+    #question = input('Поиск: ')
+    #search_line = '%20'.join(question.split())
+    url = 'https://www.avito.ru/ekaterinburg/sport_i_otdyh/zimnie_vidy_sporta?cd=1&q='+search_line
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.content.decode('utf-8'), "html.parser")
+    search = soup.find_all('div', {'class': 'snippet-horizontal item item_table clearfix js-catalog-item-enum item-with-contact js-item-extended'})
+
+    search_list = []
+    for items in search:
+        items_list = []
+
+        #Ищем внутри одного из div-ов внутренний div и достаем текст ссылки
+        name = items.find('div', {'class': 'snippet-title-row'}).find('a').text
+        strip_name = name.strip('\n ')
+        items_list.append(strip_name)
+
+        #Ищем цену, делим строку, чтобы избавиться от символа рубля и собираем в число
+        price = items.find('span', {'class': 'price'}).text
+        split_price = price.split()
+        clear_price = ''.join(split_price[:-1])
+        items_list.append(int(clear_price))
+
+        #Ищем внутри одного из div-ов внутренний div, в нем атрибут а, и из него вытаскиваем ссылку
+        href = items.find('div', {'class': 'snippet-title-row'}).find('a').get('href')
+        items_list.append(href)
+
+        #Добавляем дату
+        items_list.append(date)
+
+        #Добавляем все в итоговый список
+        search_list.append(items_list)
+    return search_list
+
+
+if __name__ == "__main__":
+    print(ParserFromAvito(Question()))
+
 
 #with open('/home/padavan/test_avito0512.html', 'w') as output_file:
 #    output_file.write(' '.join(search_list))
